d7260a4b028576cc4943425b92d746574262c65c
ProblemPrincipal <3653544699@qq.com>
2026-09-02 11:12:43 +0800

    feat(map): 添加构建期双引擎架构

--- src/map/useMapController.ts
+++ src/map/useMapController.ts
@@ -1,12 +1,10 @@
 import { inject, provide, type InjectionKey } from "vue"
 import { MapController } from "./mapController"
-import { createCesiumMapEngine } from "./engineProvider"
-import type { MapEngineFactory } from "./types"
 
 export const mapControllerKey: InjectionKey<MapController> = Symbol("MapController")
 
-export function provideMapController(createEngine: MapEngineFactory = createCesiumMapEngine) {
-  const controller = new MapController(createEngine)
+export function provideMapController() {
+  const controller = new MapController()
 
   provide(mapControllerKey, controller)
 
